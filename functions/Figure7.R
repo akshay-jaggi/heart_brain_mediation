e1ee7dbd52e34c744c541efe5de0f1ba663c105b
Akshay Jaggi <akshay.x.jaggi@gmail.com>
2022-06-20 21:06:39 -0400

    Making progress

--- functions/Figure7.R
+++ functions/Figure7.R
@@ -19,7 +19,7 @@ library("ggplot2")
 library("paletteer")
 
 sort.by.ies = function(dataframe) {
-  ies = dataframe[dataframe$label=='ie',]
+  ies = dataframe[grepl('ie', dataframe$label),]
   ies.brain = ies[grep('brain', ies$mediator),]
   ies.heart = ies[grep('heart', ies$mediator),]
   sorting = c(ies.brain[order(ies.brain$est.std, decreasing=TRUE),]$mediator, 
@@ -27,8 +27,7 @@ sort.by.ies = function(dataframe) {
   return(sorting)
 }
 
-direct.indirect.plot = function(plot, independent, dependent, sorting, ylims=c(-0.1,0.05)) {
-  plot = plot[plot$independent==independent & plot$dependent==dependent,]
+direct.indirect.plot = function(plot, sorting, ylims=c(-0.1,0.05)) {
   mycolors <- (values = c( # BRAIN IDP COLOUR ASSIGNMENTS  -----
                            
                            "#023FA5FF", # MO
@@ -149,4 +148,121 @@ direct.indirect.plot = function(plot, independent, dependent, sorting, ylims=c(-
   return(x)
 }
 
-
+indirect.plot = function(plot, sorting, ylims=c(-0.1,0.05)) {
+  mycolors <- (values = c( # BRAIN IDP COLOUR ASSIGNMENTS  -----
+                           
+                           "#023FA5FF", # MO
+                           "#455AA7FF", # ICVF
+                           "#3C5488B2", # ISOVF
+                           "#6673B1FF", # l2
+                           "#828BBBFF", # MD
+                           "#9BA1C5FF", # OD
+                           "#8796C2FF", # l1
+                           "#95AFD0FF", # FA
+                           "#A7C6DDFF", # L3
+                           "#7695f0",   # Volume (note consider revising- too blue?)
+                           
+                           # CARDIAC MRI IDP COLOUR ASSIGNMENTS  -----
+                           
+                           "#DED3D5FF", # Global variance
+                           "#C8969FFF", # Local uniformity
+                           "#BD7B88FF", # Shape
+                           "#AF5E70FF", # Size
+                           "#A03C56FF", # Local Dimness
+                           "#8E063BFF"  # Global intensity
+  ))
+  # Create column indicating whether heart or brain latent factor -----
+  plot  <-
+    plot %>% mutate(
+      type =
+        case_when(
+          grepl("heart", mediator) ~ "Heart",
+          grepl("brain", mediator) ~ "Brain",
+          TRUE ~ "All"
+        )
+    )
+  plot <- plot %>% filter(grepl('ie',label))
+  
+  # Rename some label column metrics for clarity
+  # (e.g. de -> direct effect and ie -> indirect effect) -----
+  plot$label <- rep('indirect effect', length(plot$label)) 
+  
+  
+  ### For tidy y-axis, remove underscores on mediator column -----
+  plot$mediator <- stringr::str_replace_all(plot$mediator,
+                                            c("_" = " ",
+                                              "pc" = "PC",
+                                              "cc" = "CC"
+                                            ))
+  sorting = stringr::str_replace_all(sorting,
+                                     c("_" = " ",
+                                       "pc" = "PC",
+                                       "cc" = "CC"
+                                     ))
+  
+  ## new order, group by type of dimensionality reduction method?
+  # Manual reorder note that setting x = reorder(mediator,-est.std) works just as well -----
+  plot  <- plot  %>%
+    mutate(mediator = fct_relevel(mediator, sorting))
+  
+  
+  #### GEOM POINT PLOT -----
+  #### note: if you want the results displayed horizontally, #coord_flip, and include:
+  ####        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  ####        We have manually assigned an order to these point-estimates, but
+  ####        x = reorder(mediator,-est.std) works too
+  
+  x <- ggplot(
+    plot,
+    aes(
+      x = mediator ,
+      y = est.std,
+      colour = mediator ,
+      group = type,
+      shape = significant
+    )
+  ) +
+    
+    geom_point(position = position_dodge(width = 0.9),
+               size = 2.2,
+               stroke = 0.9) +
+    
+    geom_errorbar(
+      aes(ymin = ci.lower,
+          ymax = ci.upper),
+      position = position_dodge(0.9),
+      width = 0.3,
+      colour = "darkgrey",
+      # colour = "#414141",
+      alpha = 0.9,
+      size = 0.8
+    ) +
+    
+    geom_hline(yintercept = 0, linetype = "dotted") +
+    
+    coord_flip() +
+    theme_bw() +
+    xlab("") +
+    ylab("") +
+    ylim(ylims[1],ylims[2]) +
+    facet_grid( ~ label)
+  
+  x <- x +
+    theme(legend.position="none",
+          axis.title.x = element_text(
+            size = 11,
+            face = "bold",
+            colour = "black"
+          ),
+          axis.title.y = element_text(
+            size = 11,
+            face = "bold",
+            colour = "black"
+          )
+    ) +
+    scale_shape_manual(values = c(1,
+                                  16)) +
+    scale_colour_manual(values = mycolors)
+  
+  return(x)
+}
