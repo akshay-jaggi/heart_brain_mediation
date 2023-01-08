76aacc9d8f822970065616309048a3b922554f83
Akshay Jaggi <akshay.x.jaggi@gmail.com>
2023-01-08 17:38:03 -0500

    Lots of changes in preparation for Nat Com

--- functions/med.plots.R
+++ functions/med.plots.R
@@ -27,6 +27,13 @@ sort.by.ies = function(dataframe) {
   return(sorting)
 }
 
+sort.by.brain = function(dataframe) {
+  ies = dataframe[grepl('ie2', dataframe$label),]
+  ies.brain = ies[grep('brain', ies$mediator),]
+  sorting = rev(ies.brain$mediator) #ies.brain[order(ies.brain$mediator, decreasing=TRUE),]$mediator
+  return(sorting)
+}
+
 direct.indirect.plot = function(plot, sorting, ylims=c(-0.1,0.05)) {
   mycolors <- (values = c( # BRAIN IDP COLOUR ASSIGNMENTS  -----
                            
@@ -263,6 +270,119 @@ indirect.plot = function(plot, sorting, ylims=c(-0.1,0.05)) {
     ) +
     scale_shape_manual(values = c(1,
                                   16)) +
+    scale_colour_manual(values = mycolors)
+  
+  return(x)
+}
+
+sequential.plot = function(plot, sorting, ie.num, ylims=c(-0.1,0.05)) {
+  mycolors <- (values = c( # BRAIN IDP COLOUR ASSIGNMENTS  -----
+                           
+                           "#023FA5FF", # MO
+                           "#455AA7FF", # ICVF
+                           "#3C5488B2", # ISOVF
+                           "#6673B1FF", # l2
+                           "#828BBBFF", # MD
+                           "#9BA1C5FF", # OD
+                           "#8796C2FF", # l1
+                           "#95AFD0FF", # FA
+                           "#A7C6DDFF", # L3
+                           "#7695f0",   # Volume (note consider revising- too blue?)
+                           
+                           # CARDIAC MRI IDP COLOUR ASSIGNMENTS  -----
+                           
+                           "#DED3D5FF", # Global variance
+                           "#C8969FFF", # Local uniformity
+                           "#BD7B88FF", # Shape
+                           "#AF5E70FF", # Size
+                           "#A03C56FF", # Local Dimness
+                           "#8E063BFF"  # Global intensity
+  ))
+  # Create column indicating whether heart or brain latent factor -----
+  plot <- plot %>% filter(grepl(paste('ie',ie.num,sep=''),label))
+  
+  # Rename some label column metrics for clarity
+  # (e.g. de -> direct effect and ie -> indirect effect) -----
+  plot$label <- rep(paste('indirect effect ',ie.num,sep=''), length(plot$label)) 
+  
+  
+  ### For tidy y-axis, remove underscores on mediator column -----
+  plot$mediator <- stringr::str_replace_all(plot$mediator,
+                                            c("_" = " ",
+                                              "pc" = "PC",
+                                              "cc" = "CC"
+                                            ))
+  sorting = stringr::str_replace_all(sorting,
+                                     c("_" = " ",
+                                       "pc" = "PC",
+                                       "cc" = "CC"
+                                     ))
+  
+  ## new order, group by type of dimensionality reduction method?
+  # Manual reorder note that setting x = reorder(mediator,-est.std) works just as well -----
+  plot  <- plot  %>%
+    mutate(mediator = fct_relevel(mediator, sorting))
+  
+  
+  #### GEOM POINT PLOT -----
+  #### note: if you want the results displayed horizontally, #coord_flip, and include:
+  ####        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  ####        We have manually assigned an order to these point-estimates, but
+  ####        x = reorder(mediator,-est.std) works too
+  
+  x <- ggplot(
+    plot,
+    aes(
+      x = mediator ,
+      y = est.std,
+      colour = mediator ,
+      shape = significant
+    )
+  ) +
+    
+    geom_point(position = position_dodge(width = 0.9),
+               size = 2.2,
+               stroke = 0.9) +
+    
+    geom_errorbar(
+      aes(ymin = ci.lower,
+          ymax = ci.upper),
+      position = position_dodge(0.9),
+      width = 0.3,
+      colour = "darkgrey",
+      # colour = "#414141",
+      alpha = 0.9,
+      size = 0.8
+    ) +
+    
+    geom_hline(yintercept = 0, linetype = "dotted") +
+    
+    coord_flip() +
+    theme_bw() +
+    xlab("") +
+    ylab("") +
+    ylim(ylims[1],ylims[2]) +
+    facet_grid( ~ label)
+  if(!all(plot$significant)) {
+    shape.scale = c(1,16)
+  } else {
+    shape.scale = c(16,1)
+  }
+  x <- x +
+    theme(legend.position="none",
+          axis.title.x = element_text(
+            size = 11,
+            face = "bold",
+            colour = "black"
+          ),
+          axis.title.y = element_text(
+            size = 11,
+            face = "bold",
+            colour = "black"
+          ),
+          aspect.ratio = 5/4
+    ) +
+    scale_shape_manual(values = shape.scale) +
     scale_colour_manual(values = mycolors)
   
   return(x)
--- functions/mediation.R
+++ functions/mediation.R
@@ -78,6 +78,68 @@ run.multiple.mediation = function(dataframe, independent, dependent, mediators, 
   return(mm.out)
 }
 
+run.multiple.parallel.mediation = function(dataframe, independent, dependent, mediators, covariates) {
+  results.list = list()
+  effects = 
+  "de := c
+  ie1 := a1*b1
+  ie2 := a2*b2
+  pm1 := (a1*b1) / (c+a1*b1+a2*b2)
+  pm2 := (a2*b2) / (c+a1*b1+a2*b2)
+  te := c + a1*b1 + a2*b2"
+  covariates = paste(covariates,collapse=" + ")
+  iter = 1
+  for(mediator1 in mediators) {
+    inner.list = list()
+    for(mediator2 in mediators) {
+      if (mediator1 == mediator2) {
+        next
+      }
+      med.model = paste(c(dependent," ~ c * ", independent, " + b1 * ",mediator1, " + b2 * ",mediator2," + ", covariates,"\n",
+                          mediator1," ~ a1 * ", independent, " + ", covariates, "\n",
+                          mediator2," ~ a2 * ", independent, " + ", covariates, "\n",
+                          mediator1,"~~",mediator2),
+                        collapse = "")
+      med.model = paste(c(med.model, effects), collapse = '\n')
+      med.sem = sem(med.model,dataframe)
+      out = standardizedsolution(med.sem)
+      inner.list[[mediator2]] = out[out$op=="~"|out$op==":=",]
+    }
+    results.processed = bind_rows(inner.list, .id = "mediator2")
+    results.processed = cbind(mediator1 = rep(mediator1, nrow(results.processed)), results.processed)
+    results.list[[mediator1]] =  results.processed
+  }
+  results.frame = dplyr::bind_rows(results.list)
+  return(results.frame)
+}
+
+run.multiple.sequential.mediation = function(dataframe, independent, dependent, mediators, covariates) {
+   effects = 
+     "de := c
+      ie1 := a1*b1
+      ie2 := a2*b2
+      ie3 := a1*m*b2
+      te  := c + a1*b1 + a2*b2 + a1*m*b2"
+    mediator1 = mediators[2]
+    covariates = paste(covariates,collapse=" + ")
+    results.list = list()
+    for(mediator2 in mediators) {
+      if(mediator1 == mediator2) {
+        next
+      }
+      med.model = paste(c(mediator1," ~ a1 * ", independent, " + ", covariates, "\n",
+                          mediator2," ~ a2 * ", independent,  " + m * ",mediator1, " + ", covariates, "\n",
+                          dependent, " ~ c * ", independent, " + b1 * ",mediator1," + b2 * ",mediator2," + ", covariates),
+                        collapse = "")
+      med.model = paste(c(med.model, effects), collapse = '\n')
+      med.sem = sem(med.model,dataframe)
+      out = standardizedsolution(med.sem)
+      results.list[[mediator2]] = out[out$op=="~"|out$op==":=",]
+    }
+    results.frame = bind_rows(results.list, .id = "mediator2")
+    return(results.frame)
+}
+
 mediation.ind.dep.pairs = function(dataframe, independents, dependents, mediators, covariates) {
   mediation.individual.single.pairs = list()
   iter = 1
@@ -90,11 +152,10 @@ mediation.ind.dep.pairs = function(dataframe, independents, dependents, mediator
       lin.model.sem = sem(lin.model,dataframe)
       out = standardizedsolution(lin.model.sem)
       if(out$pvalue[1] < 0.05) {
-        mediation.individual.single.pair = run.single.mediation(dataframe,
+        mediation.individual.single.pair.final = run.single.mediation(dataframe,
                                                                 independent,
                                                                 dependent,
                                                                 mediators, covariates.drop)
-        mediation.individual.single.pair.final = process.mediation.results(mediation.individual.single.pair)
         mediation.individual.single.pair.final$independent = rep(independent, nrow(mediation.individual.single.pair.final))
         mediation.individual.single.pair.final$dependent = rep(dependent, nrow(mediation.individual.single.pair.final))
         mediation.individual.single.pairs[[iter]] = mediation.individual.single.pair.final
